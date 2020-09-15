d4de77a7d86903f6ceb4af2878e2c858f16254cf
Euijae Kim <Euijae.Kim@nike.com>
2020-09-15 01:26:00 -0700

    new version kicked off

---
+++ src/Notebook.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Notebook = () => (
+  <div class="ui grid fluid notebook-grid">
+    <div class="three wide column notebook-column">
+    </div>
+    <div class="nine wide column notebook-column">
+    </div>
+    <div class="four wide column notebook-column">
+    </div>
+  </div>
+);
+
+export default Notebook;
