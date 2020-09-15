a6458c8a2e3fdf9ad461076f8a3cd5349825696f
Euijae Kim <Euijae.Kim@nike.com>
2020-09-15 01:36:53 -0700

    second

--- src/Notebook.js
+++ src/Notebook.js
@@ -1,13 +1,452 @@
 import React from 'react';
 import './styles.css';
 
+const styles = {
+  notebookColumn: {
+    height: '100vh',
+    overflow: 'auto'
+  },
+  notebookGrid: {
+    margin: '0 auto'
+  },
+  black: {
+    backgroundColor: 'black'
+  }
+};
+
+const {notebookGrid, notebookColumn, black} = styles;
+
 const Notebook = () => (
-  <div class="ui grid fluid notebook-grid">
-    <div class="three wide column notebook-column">
+  <div class="ui grid fluid" style={notebookGrid}>
+    <div class="three wide column black" style={notebookColumn}>
+    <div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
     </div>
-    <div class="nine wide column notebook-column">
     </div>
-    <div class="four wide column notebook-column">
+    <div class="nine wide column" style={notebookColumn}>
+    <div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+<div class="ui bulleted list">
+  <div class="item">Gaining Access</div>
+  <div class="item">Inviting Friends</div>
+  <div class="item">
+    <div>Benefits</div>
+    <div class="list">
+      <a class="item" href="#">Link to somewhere</a>
+      <div class="item">Rebates</div>
+      <div class="item">Discounts</div>
+    </div>
+  </div>
+  <div class="item">Warranty</div>
+</div>
+    </div>
+    <div class="four wide column" style={notebookColumn}>
+    <div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div><div class="ui bulleted list">
+      <div class="item">Gaining Access</div>
+      <div class="item">Inviting Friends</div>
+      <div class="item">
+        <div>Benefits</div>
+        <div class="list">
+          <a class="item" href="#">Link to somewhere</a>
+          <div class="item">Rebates</div>
+          <div class="item">Discounts</div>
+        </div>
+      </div>
+      <div class="item">Warranty</div>
+    </div>
     </div>
   </div>
 );
