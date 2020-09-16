306c45d5d930ec21aca998e70b5a463aa6474e73
Euijae Kim <euijaekim@Euijaes-MacBook-Air.local>
2020-09-16 00:56:22 -0700

    9.14

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div class="ui center aligned container">
+    Made with <i aria-hidden="true" class="heart icon red" />in <b>Oregon</b>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Divider from './Divider';
+import Footer from './Footer';
+
+// <img src={require('../image/download.png')} alt="" className="ui image avatar rounded left floated" />
+class Menu extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="ui inverted segment">
+          <h1 class="ui left floated header">Notebook</h1>
+          <Divider />
+          <div class="ui link list inverted">
+            <a class="ui header item" style={{color: 'white'}}>What's new</a>
+          </div>
+          <Divider />
+          <div class="ui link list inverted">
+            <a class="ui header item" style={{color: 'white'}}>Status Planning</a>
+          </div>
+          <Divider />
+          <div class="ui link list inverted">
+            <div class="ui header item" style={{color: 'white'}}>Language</div>
+            <a class="active item" style={{borderRight: '5px solid #134A8E'}}>Redux</a>
+            <a class="item">Python</a>
+          </div>
+          <Divider />
+          <div class="ui link list inverted">
+            <div class="ui header item" style={{color: 'white'}}>Application</div>
+            <a class="item">Autocomplete</a>
+            <a class="item">Spell Checker</a>
+          </div>
+          <Divider />
+          <Footer />
+        </div>
+      </div>
+    );
+  };
+}
+
+export default Menu;
