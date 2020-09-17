94f7e4dbadfc9dcb00b5733e92b592139c0d0b7e
Euijae Kim <euijaekim@Euijaes-MacBook-Air.local>
2020-09-16 22:54:15 -0700

    add my ws

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Footer = () => (
-  <div class="ui center aligned container">
-    Made with <i aria-hidden="true" class="heart icon red" />in <b>Oregon</b>
-  </div>
+  <>
+    <div class="ui center aligned container">
+      Made with <i aria-hidden="true" class="heart icon red" />in <b>Oregon</b>
+    </div>
+    <div class="ui center aligned container">
+      Copyright <i aria-hidden="true" class="copyright open icon blue" />
+      2020 <b><a style={{color: 'white'}} href="http://www.euijae.io" target="_blank">Euijae</a></b>
+    </div>
+  </>
 );
 
 export default Footer;
