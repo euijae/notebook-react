8a1150fd8de04e6ca644f5132de77a4417abc145
Euijae Kim <euijaekim@Euijaes-MacBook-Air.local>
2020-09-16 22:35:22 -0700

    change locatio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div class="ui center aligned container">
-    Made with <i aria-hidden="true" class="heart icon red" />in <b>Beaverton, OR</b>
+    Made with <i aria-hidden="true" class="heart icon red" />in <b>Oregon</b>
   </div>
 );
 
