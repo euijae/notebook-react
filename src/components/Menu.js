fee0db0f39419f2f807a2fade8b221d37766cfcd
Euijae Kim <euijaekim@Euijaes-MacBook-Air.local>
2020-09-16 22:35:51 -0700

    use js logic to create list

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,42 +2,62 @@ import React from 'react';
 import Divider from './Divider';
 import Footer from './Footer';
 
+import '../styles.css'
+import menu from '../seed/menu';
+
 // <img src={require('../image/download.png')} alt="" className="ui image avatar rounded left floated" />
 class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu
+    }
+  }
+
+  menuTitleGenerator(menuItems) {
+    const { title } = menuItems;
+    return (
+      <>
+        <h1 className="ui left floated header">{title}</h1>
+        <Divider />
+      </>
+    );
+  }
+
+  menuListGenerator(menuItems) {
+    const { sections } = menuItems;
+    return sections.map(section => (
+      <div className="ui link list inverted">
+        {titleCreator(section)}
+        {contentsCreator(section)}
+        <Divider />
+      </div>
+    ));
+
+    function titleCreator(section) {
+      const {title, clickable} = section.header;
+
+      if(clickable) {
+        return (<a className="ui header item" style={{color: 'white'}}>{title}</a>)
+      } else {
+        return (<div className="ui header item" style={{color: 'white'}}>{title}</div>)
+      }
+    }
+
+    function contentsCreator(section) {
+      const {contents} = section;
+      return (contents || []).map(content => (
+        <a className="item">{content.title}</a>
+      ));
+    }
+  }
+
   render() {
     return (
       <div>
-        <div class="ui inverted segment">
-          <h1 class="ui left floated header">Notebook</h1>
-          <Divider />
-          <div class="ui link list inverted">
-            <a class="ui header item" style={{color: 'white'}}>What's new</a>
-          </div>
-          <Divider />
-          <div class="ui link list inverted">
-            <a class="ui header item" style={{color: 'white'}}>Status Planning</a>
-          </div>
-          <Divider />
-          <div class="ui link list inverted">
-            <div class="ui header item" style={{color: 'white'}}>Language</div>
-            <a class="active item" style={{borderRight: '5px solid #134A8E'}}>Redux</a>
-            <a class="item">Python</a>
-          </div>
-          <Divider />
-          <div class="ui link list inverted">
-            <div class="ui header item" style={{color: 'white'}}>Application</div>
-            <a class="item">Autocomplete</a>
-            <a class="item">Spell Checker</a>
-            <a class="item">Image Processing</a>
-            <a class="item">Voice Recognition</a>
-          </div>
-          <Divider />
-          <div class="ui link list inverted">
-            <div class="ui header item" style={{color: 'white'}}>Mathematics</div>
-            <a class="item">Combinatorics</a>
-            <a class="item">Number Theory</a>
-          </div>
-          <Divider />
+        <div className="ui inverted segment">
+          {this.menuTitleGenerator(this.state.menu)}
+          {this.menuListGenerator(this.state.menu)}
           <Footer />
         </div>
       </div>
