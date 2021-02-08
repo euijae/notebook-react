bc96a7caa75d82e5180de749eb3db17c3c87392a
euijae <acreclan49@gmail.com>
2021-02-07 16:43:36 -0800

    dynamic year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     </div>
     <div class="ui center aligned container">
       Copyright <i aria-hidden="true" class="copyright open icon blue" />
-      2020 <b><a style={{color: 'white'}} href="http://www.euijae.io" target="_blank">Euijae</a></b>
+      {new Date().getFullYear()} <b><a style={{color: 'white'}} href="http://www.euijae.io" target="_blank">Euijae</a></b>
     </div>
   </>
 );
